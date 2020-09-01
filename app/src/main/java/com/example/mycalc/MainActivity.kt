8e6ac308e3d998f6cb58e9cf82933d2ad0188119
iNapalm <apoloninoleg@gmail.com>
2020-09-01 18:10:37 +0300

    ver 3.0

--- app/src/main/java/com/example/mycalc/MainActivity.kt
+++ app/src/main/java/com/example/mycalc/MainActivity.kt
@@ -2,38 +2,37 @@ package com.example.mycalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private var res: ArrayList<String> = ArrayList()
-    private var sum: String = ""
+    private var sum: String = "0"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_1.setOnClickListener { setTextCalcHead(btn_1.text as String) }
-        btn_2.setOnClickListener { setTextCalcHead(btn_2.text as String) }
-        btn_3.setOnClickListener { setTextCalcHead(btn_3.text as String) }
-        btn_4.setOnClickListener { setTextCalcHead(btn_4.text as String) }
-        btn_5.setOnClickListener { setTextCalcHead(btn_5.text as String) }
-        btn_6.setOnClickListener { setTextCalcHead(btn_6.text as String) }
+        btn_1.setOnClickListener { setTextCalcHead(btn_1.text.toString()) }
+        btn_2.setOnClickListener { setTextCalcHead(btn_2.text.toString()) }
+        btn_3.setOnClickListener { setTextCalcHead(btn_3.text.toString()) }
+        btn_4.setOnClickListener { setTextCalcHead(btn_4.text.toString()) }
+        btn_5.setOnClickListener { setTextCalcHead(btn_5.text.toString()) }
+        btn_6.setOnClickListener { setTextCalcHead(btn_6.text.toString()) }
 
         btn_sum.setOnClickListener {
             res.add(sum)
 
-            setTextCalcHead(btn_sum.text as String)
+            setTextCalcHead(btn_sum.text.toString())
         }
 
         btn_result.setOnClickListener {
-            var i:Int = 0
+            var i:Long = 0
             res.add(sum)
 
             for (key in res) {
-                i += key.toInt()
+                i += key.toLong()
             }
             sum=i.toString()
             calc_head.text = i.toString()
@@ -45,6 +44,10 @@ class MainActivity : AppCompatActivity() {
             val str = calc_head.text.toString()
             if (str.isNotEmpty()) {
                 calc_head.text = str.substring(0, str.length - 1)
+                sum = str.substring(0, str.length - 1)
+            }
+            else {
+                sum = "0"
             }
         }
 
@@ -52,7 +55,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setTextCalcHead(str: String) {
         sum = if (str != "+") sum + str
-        else ""
+        else "0"
 
         calc_head.append(str)
     }
